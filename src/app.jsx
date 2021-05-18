96e21bf3f82a02e1e8f497326343bbe286e38d91
Jongwoop <58589069+Jongwoop@users.noreply.github.com>
2021-05-18 17:09:51 +0300

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,26 @@
 import "./app.css";
-import Habit from "../../habit-tracker/src/habit";
+import React, { useEffect, useState } from "react";
+import VideoList from "./components/video_list/video_list";
+// import Navbar from "./components/navbar";
+// import Videos from "./components/videos";
 
 function App() {
-  return <h1>Hello</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBqGnA_ht9qwy199YrFC83xITAtBS__8KQ",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+  return <VideoList videos={videos} />;
 }
 export default App;
