8c6f6451affd24c3e8a037400454e364f8066aee
Jongwoop <58589069+Jongwoop@users.noreply.github.com>
2021-05-21 00:28:42 +0300

    bac to the list when searching

--- src/app.jsx
+++ src/app.jsx
@@ -2,14 +2,23 @@ import styles from "./app.module.css";
 import React, { useEffect, useState } from "react";
 import VideoList from "./components/video_list/video_list";
 import SearchHeader from "./components/search_header/search_header";
+import VideoDetail from "./components/video_detail/video_detail";
 // import Videos from "./components/videos";
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
   const search = (query) => {
+    setSelectedVideo(null);
     youtube //
       .search(query)
-      .then((videos) => setVideos(videos));
+      .then((videos) => {
+        setVideos(videos);
+      });
   };
 
   useEffect(() => {
@@ -20,7 +29,20 @@ function App({ youtube }) {
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
+        </div>
+      </section>
     </div>
   );
 }
